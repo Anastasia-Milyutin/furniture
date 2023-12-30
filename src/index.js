a55b4bc0cd6b81a1a4b92a5220571027823a881b
Anastasia-Milyutin <141128897+Anastasia-Milyutin@users.noreply.github.com>
2023-12-30 19:37:45 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import BrowserRouter from "react-router-dom/BrowserRouter";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 reportWebVitals();
