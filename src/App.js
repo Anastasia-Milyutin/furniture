4b6103ad587867dcf5a041e07d1bc543bb825089
Anastasia-Milyutin <141128897+Anastasia-Milyutin@users.noreply.github.com>
2023-12-31 14:06:36 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Header from './Header';
+import Welcome from './Welcome'
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Welcome/>
     </div>
   );
 }
